b5d6fe5d998fc5bc720a9f5b698ca537ec5d06bd
ac4370879@gmail.com <ac4370879@gmail.com.>
2026-04-03 09:49:53 +0530

    Add

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req ,res)=>{
+   res.send("Api is running");
+})
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
